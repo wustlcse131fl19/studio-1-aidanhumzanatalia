639beb929061863f2d14e826624b35016996311a
humzasiddiqui <hsiddiqui@wustl.edu>
2019-08-29 16:55:22 -0500

    Studio 1 done

--- src/studio1/HiFour.java
+++ src/studio1/HiFour.java
@@ -9,13 +9,14 @@ import support.cse131.ArgsProcessor;
 public class HiFour {
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
-		String s0 = ap.nextString("Enter value for arg 0");
-		String s1 = ap.nextString("Enter value for arg 1");
-		String s2 = ap.nextString("Enter value for arg 2");
-		String s3 = ap.nextString("Enter value for arg 3");
+		String s0 = ap.nextString("Friend 1 name");
+		String s1 = ap.nextString("Friend 2 name");
+		String s2 = ap.nextString("Friend 3 name");
+		String s3 = ap.nextString("Friend 4 name");
 		//
 		// Say hello to the names in s0 through s3.
 		//
-
+		String output = "Greetings " + s0 + ", " + s1 + ", " + s2 + ", " + "and " + s3 + ". " + "Y'all lookin cute today!!" ;
+		System.out.println(output) ;
 	}
 }
